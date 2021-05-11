bd6f7063747e8637d70e6c2b9d089e399ade8a91
Your Name <you@example.com>
2021-05-11 23:07:01 +0700

    process.env.PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const usernames = [];
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 console.log("Server Running");
 
 app.get("/", (req, res) => {
